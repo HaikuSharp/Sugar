5b69735e222b0b9bf274e79a04e38723cef12de4
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-02 23:03:17 +0300

    Add Non-Generic Cast

--- src/Sugar.Object.Extensions/ObjectCastExtensions.cs
+++ src/Sugar.Object.Extensions/ObjectCastExtensions.cs
@@ -1,5 +1,23 @@
 ﻿namespace Sugar.Object.Extensions;
 public static class ObjectCastExtensions {
+ // For Classes/Interfaces.
+ extension(object source) {
+  public bool Is<TCast>() {
+   return source is TCast;
+  }
+  public bool Is<TCast>(out TCast cast) {
+   if(source is TCast c) {
+    cast = c;
+    return true;
+   }
+   cast = default(TCast);
+   return false;
+  }
+  public TCast As<TCast>() where TCast : class {
+   return source as TCast;
+  }
+ }
+ // For Structures. (Box/Unbox Optimization)
  extension<TSource>(TSource source) {
   public bool Is<TCast>() {
    return source is TCast;
