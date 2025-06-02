aa59eefe8caeeac9325bff937f8057bb1d8a9ea8
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-02 13:52:33 +0300

    Add Add Aliases (Null Check Methods)

--- src/Sugar.Object.Extensions/ObjectNullExtensions.cs
+++ src/Sugar.Object.Extensions/ObjectNullExtensions.cs
@@ -1,8 +1,14 @@
 ﻿namespace Sugar.Object.Extensions;
 public static class ObjectNullExtensions {
  extension<TSource>(TSource source) where TSource : class {
-  public bool IsNil() {
+  public bool IsNull() {
    return source is null;
+  }
+  public bool IsNotNull() {
+   return !source.IsNull();
+  }
+  public bool IsNil() {
+   return source.IsNull();
   }
   public bool IsNotNil() {
    return !source.IsNil();
