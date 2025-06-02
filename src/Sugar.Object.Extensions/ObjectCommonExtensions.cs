8a1e3285dc6aab978599864a06567b623768e512
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-02 21:28:52 +0300

    Fix ObjectCommonExtensions
    Remove where TSource : class

--- src/Sugar.Object.Extensions/ObjectCommonExtensions.cs
+++ src/Sugar.Object.Extensions/ObjectCommonExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sugar.Object.Extensions;
 public static class ObjectCommonExtensions {
- extension<TSource>(TSource source) where TSource : class {
+ extension<TSource>(TSource source) {
   public void Forget() {
    // Nothing...
   }
