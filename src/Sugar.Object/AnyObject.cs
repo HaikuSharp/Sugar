a21deea514293a60a8b2c2d2ef759ea96f826ae0
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-02 14:31:48 +0300

    Any Object

---
+++ src/Sugar.Object/AnyObject.cs
@@ -0,0 +1,36 @@
+﻿namespace Sugar.Object;
+public class AnyObject {
+ private static AnyObject s_Any;
+ public static AnyObject Any {
+  get {
+   return s_Any ??= new();
+  }
+ }
+ public override bool Equals(object obj) {
+  return true;
+ }
+ public override int GetHashCode() {
+  return 0;
+ }
+ public override string ToString() {
+  return "any";
+ }
+ public static bool operator ==(AnyObject a, AnyObject b) {
+  return true;
+ }
+ public static bool operator !=(AnyObject a, AnyObject b) {
+  return false;
+ }
+ public static bool operator ==(AnyObject a, object b) {
+  return true;
+ }
+ public static bool operator !=(AnyObject a, object b) {
+  return false;
+ }
+ public static bool operator ==(object a, AnyObject b) {
+  return true;
+ }
+ public static bool operator !=(object a, AnyObject b) {
+  return false;
+ }
+}
