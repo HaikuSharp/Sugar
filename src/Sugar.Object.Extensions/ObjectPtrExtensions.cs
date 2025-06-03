3ee4d9b919bc8205c9cefa7cda44589f443cb760
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-03 14:52:09 +0300

    Add ObjectPtrExtensions

---
+++ src/Sugar.Object.Extensions/ObjectPtrExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace Sugar.Object.Extensions;
+public static class ObjectPtrExtensions {
+#pragma warning disable CS8500 // &ws принимает адрес, получает размер или объявляет указатель на управляемый тип
+ [MethodImpl(MethodImplOptions.AggressiveInlining)]
+ public static unsafe IntPtr GetObjectPtr(this object obj) {
+  var ws = new WrappingStruct();
+  ws.obj = obj;
+  var wsPtr = &ws;
+  var objPtr = *(IntPtr*)wsPtr;
+  return objPtr;
+ }
+ [MethodImpl(MethodImplOptions.AggressiveInlining)]
+ public static unsafe IntPtr GetTypeHandlePtr(this object obj) {
+  var ws = new WrappingStruct();
+  ws.obj = obj;
+  var wsPtr = &ws;
+  var typePtr = **(IntPtr**)wsPtr;
+  return typePtr;
+ }
+#pragma warning restore CS8500
+ private struct WrappingStruct {
+  public object obj;
+ }
+}
